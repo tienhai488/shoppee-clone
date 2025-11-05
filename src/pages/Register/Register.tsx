f91ebc617dd48921fc74c741137a38a8bde9a940
Le Tien Hai <tienhaile488@gmail.com>
2025-11-05 23:34:15 +0700

    feat: validate register form

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,36 +1,94 @@
-import { Link } from "react-router-dom";
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { getRules } from 'src/utils/rules'
+
+interface FormData {
+  email: string
+  password: string
+  password_confirmation: string
+}
 
 export default function Register() {
-  return <div className="bg-orange-600">
-    <div className="max-w-7xl mx-auto px-4">
-      <div className="grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10">
-        <div className="lg:col-span-2 lg:col-start-4">
-          <form className="p-10 rounded bg-white shadow-sm">
-            <div className="text-2xl">Đăng Ký</div>
-            <div className="mt-8">
-              <input type="email" name="email" id="email" className="p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm" placeholder="Email" />
-              <div className="mt-1 text-red-600 min-h-[1rem] text-sm">Email không hợp lệ</div>
-            </div>
-            <div className="mt-3">
-              <input type="password" name="password" id="password" className="p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm" placeholder="Mật khẩu" />
-              <div className="mt-1 text-red-600 min-h-[1rem] text-sm">Mật khẩu không hợp lệ</div>
-            </div>
-            <div className="mt-3">
-              <input type="password" name="password_confirm" id="password_confirm" className="p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm" placeholder="Xác nhận mật khẩu" />
-              <div className="mt-1 text-red-600 min-h-[1rem] text-sm">Mật khẩu không hợp lệ</div>
-            </div>
-            <div className="mt-3">
-              <button className="w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600">
-                Đăng Ký
-              </button>
-            </div>
-            <div className="flex items-center justify-center mt-8">
-              <span className="text-slate-400">Bạn đã có tài khoản? </span>
-              <Link to="/login" className="text-red-500 ml-1">Đăng Nhập</Link>
-            </div>
-          </form>
+  const {
+    register,
+    handleSubmit,
+    watch,
+    getValues,
+    formState: { errors }
+  } = useForm<FormData>()
+
+  const onSubmit = handleSubmit(
+    (data) => {
+      console.log(data)
+    },
+    (data) => {
+      const password = getValues('password')
+      console.log('password', password)
+
+      console.log('errors', data)
+    }
+  )
+
+  const rules = getRules<FormData>(getValues)
+
+  const formValues = watch()
+  console.log('form values', formValues)
+
+  return (
+    <div className='bg-orange-600'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10'>
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <form className='p-10 rounded bg-white shadow-sm' onSubmit={onSubmit} noValidate>
+              <div className='text-2xl'>Đăng Ký</div>
+              <div className='mt-8'>
+                <input
+                  type='email'
+                  id='email'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                  placeholder='Email'
+                  {...register('email', rules.email)}
+                />
+                <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errors.email?.message}</div>
+              </div>
+              <div className='mt-3'>
+                <input
+                  type='password'
+                  id='password'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                  placeholder='Mật khẩu'
+                  {...register('password', rules.password)}
+                />
+                <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errors.password?.message}</div>
+              </div>
+              <div className='mt-3'>
+                <input
+                  type='password'
+                  id='password_confirmation'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                  placeholder='Xác nhận mật khẩu'
+                  {...register('password_confirmation', rules.password_confirmation)}
+                />
+                <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errors.password_confirmation?.message}</div>
+              </div>
+              <div className='mt-3'>
+                <button
+                  type='submit'
+                  className='w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600'
+                >
+                  Đăng Ký
+                </button>
+              </div>
+              <div className='flex items-center justify-center mt-8'>
+                <span className='text-slate-400'>Bạn đã có tài khoản? </span>
+                <Link to='/login' className='text-red-500 ml-1'>
+                  Đăng Nhập
+                </Link>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  )
 }
---
+++ src/utils/rules.ts
@@ -0,0 +1,51 @@
+import type { UseFormGetValues, FieldValues } from "react-hook-form"
+
+export const getRules = <TFieldValues extends FieldValues>(getValues?: UseFormGetValues<TFieldValues>) => ({
+  email: {
+    required: {
+      value: true,
+      message: 'Email không được để trống'
+    },
+    pattern: {
+      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+      message: 'Email không hợp lệ'
+    },
+    minLength: {
+      value: 5,
+      message: 'Độ dài tối thiểu là 5 ký tự'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài tối đa là 160 ký tự'
+    },
+  },
+  password: {
+    required: {
+      value: true,
+      message: 'Mật khẩu không được để trống'
+    },
+    minLength: {
+      value: 6,
+      message: 'Độ dài tối thiểu là 6 ký tự'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài tối đa là 160 ký tự'
+    },
+  },
+  password_confirmation: {
+    required: {
+      value: true,
+      message: 'Mật khẩu nhập lại không được để trống'
+    },
+    minLength: {
+      value: 6,
+      message: 'Độ dài tối thiểu là 6 ký tự'
+    },
+    maxLength: {
+      value: 160,
+      message: 'Độ dài tối đa là 160 ký tự'
+    },
+    validate: typeof getValues === 'function' ? (value: any) => value === getValues('password' as any) || 'Mật khẩu nhập lại không khớp' : undefined
+  }
+})
